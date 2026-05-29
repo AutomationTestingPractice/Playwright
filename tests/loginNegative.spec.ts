b1f2e39ab3883c10297d0ba0cd7c9ad542173141
SDET_QA_Practice (AutomationTestingPractice) <rahulkumarrk2247@gmail.com>
2026-05-29 12:05:27 +0530

    negativetestcases added

--- tests/loginNegative.spec.ts
+++ tests/loginNegative.spec.ts
@@ -1,1 +1,57 @@
 
+import { test } from '@playwright/test';
+import { LoginPage } from '../pages/LoginPage';
+
+test.describe('Negative Login Tests', () => {
+
+  test('Empty Username',
+    async ({ page }) => {
+
+    const loginPage = new LoginPage(page);
+
+    await page.goto('/web/index.php/auth/login');
+
+    await loginPage.login(
+      '',
+      'Password123'
+    );
+
+    await loginPage.verifyUsernameError(
+      'Required'
+    );
+  });
+
+  test('Empty Password',
+    async ({ page }) => {
+
+    const loginPage = new LoginPage(page);
+
+    await page.goto('/web/index.php/auth/login');
+
+    await loginPage.login(
+      'testuser',
+      ''
+    );
+
+    await loginPage.verifyPasswordError(
+      'Required'
+    );
+  });
+
+  test('Invalid Credentials',
+    async ({ page }) => {
+
+    const loginPage = new LoginPage(page);
+
+    await page.goto('/web/index.php/auth/login');
+
+    await loginPage.login(
+      'wronguser',
+      'wrongpassword'
+    );
+
+    await loginPage.verifyErrorMessage(
+      'Invalid credentials'
+    );
+  });
+});
